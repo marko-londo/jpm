3204e1498cf9ecd213c92d284c7b489f2924d513
Marko <londo.py@gmail.com>
2025-07-24 14:47:17 -0400

    fix

--- internal.py
+++ internal.py
@@ -161,8 +161,8 @@ def dashboard():
     header()
     # 1. Operating zone
     zone_day = get_today_operating_zone(address_df)
-    st.markdown(f"### <span style='color:#FF8C8C;'>{zone_day} Zone</span>", unsafe_allow_html=True)
-
+    st.markdown(f"### Zone: {zone_day}")
+    
     # 2. YW (Yardwaste) zone color this week
     yw_route = get_yw_zone_color()
     color_code = "#3980ec" if yw_route == "140" else "#EAC100"
